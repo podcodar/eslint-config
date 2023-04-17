f8d91af6b7a28c7a9617d2a57a5fab641ef76179
Marco Antônio <ma.souza.junior@gmail.com>
2023-04-17 19:11:02 -0300

    enh: add core vitals next

--- packages/next/index.js
+++ packages/next/index.js
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ["@podcodar/eslint-config-base", "next", "./rules/next"],
+  extends: [
+    "@podcodar/eslint-config-base",
+    "next",
+    "next/core-web-vitals",
+    "./rules/next"
+  ],
   parserOptions: {
     babelOptions: {
       presets: [require.resolve("next/babel")],
