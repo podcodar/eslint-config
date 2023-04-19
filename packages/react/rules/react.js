9f299125e4925ee0bb985dddd2a05c7599f60b03
Marco Antônio <ma.souza.junior@gmail.com>
2023-04-19 15:23:51 -0300

    enh: improve component formatting

--- packages/react/rules/react.js
+++ packages/react/rules/react.js
@@ -1,6 +1,9 @@
 module.exports = {
   rules: {
-    "react/jsx-max-props-per-line": ["error", { "when": "multiline" }],
+    "react/jsx-sort-props": "error",
+    "react/self-closing-comp": "error",
+    "react/jsx-closing-bracket-location": "error",
     "react/jsx-first-prop-new-line": ["error", "multiline-multiprop"],
+    "react/jsx-max-props-per-line": ["error", { "maximum": { "single": 2, "multi": 1 } }],
   }
 };
