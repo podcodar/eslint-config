dfb264d80ff3226a8ed00f990aa1f4b6204ac5e8
Marco Antônio <ma.souza.junior@gmail.com>
2023-04-19 14:57:34 -0300

    enh: improve react breakdown component props

--- packages/react/rules/react.js
+++ packages/react/rules/react.js
@@ -1,5 +1,6 @@
 module.exports = {
   rules: {
-    "react/jsx-max-props-per-line": ["error"]
+    "react/jsx-max-props-per-line": ["error", { "when": "multiline" }],
+    "react/jsx-first-prop-new-line": ["error", "multiline-multiprop"],
   }
 };
