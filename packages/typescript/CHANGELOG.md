8a42a3c0568e490cdb529aae13f1ade2c002ef9f
Marco Antônio <ma.souza.junior@gmail.com>
2023-04-27 17:53:05 -0300

    oth: release 1.5.1

--- packages/typescript/CHANGELOG.md
+++ packages/typescript/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @podcodar/eslint-config-typescript
 
+## 1.5.1
+
+### Patch Changes
+
+- add parserOptions
+
 ## 1.5.0
 
 ### Minor Changes
--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -7,6 +7,7 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
+  root: true,
   parserOptions: {
     project: ["tsconfig.json"]
   }
